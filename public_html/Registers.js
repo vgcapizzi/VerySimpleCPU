174e602bf09533671dd2f0e95894713bee174811
vgcapizzi <vgcapizzi@gmail.com>
2016-03-14 17:45:20 -0400

    Added files via upload

---
+++ public_html/Registers.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function RegClass(){
+    var byte = new Array(8);
+    var totalMem = new Array(64);
+    
+}
+
+function dataReg(){
+    var address = new Array(6);
+    var inst = new Array(2);
+} 
+
+function acReg(){
+    var AC = new Array(8);
+}
+
+function pcReg(){
+    var PC = new Array(8);
+    
+}
